6ca48b90bca61c22ce3cc71d03e538b4b6d391f4
backdevhong <ghddls0704@kakao.com>
2024-01-13 20:44:03 +0900

    java to kotlin

---
+++ src/main/java/org/server/godofhacking/GodOfHacking.kt
@@ -0,0 +1,13 @@
+package org.server.godofhacking
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class GodOfHacking : JavaPlugin() {
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
